c1ee7127d44d92939c6ab4d5217ea45c9872f259
JengTing <jengting1211@hotmail.com>
2022-05-20 13:48:18 +0800

    Add：新增 [20220506.Q3 計算跨日的停車費-標準]

--- Parking/ParkingFeeBiz.cs
+++ Parking/ParkingFeeBiz.cs
@@ -1,16 +1,128 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Parking
 {
+
+    public class SingleDayFee : IEquatable<SingleDayFee>
+    {
+        public DateTime StartTime { get; set; } // 精確到分鐘的入場時間
+        public DateTime EndTime { get; set; } // 精確到分鐘的離場時間
+        public int Fee { get; set; } // 本日應收取費用
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as SingleDayFee);
+        }
+
+        public bool Equals(SingleDayFee other)
+        {
+            return other != null &&
+                   StartTime == other.StartTime &&
+                   EndTime == other.EndTime &&
+                   Fee == other.Fee;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 933646964;
+            hashCode = hashCode * -1521134295 + StartTime.GetHashCode();
+            hashCode = hashCode * -1521134295 + EndTime.GetHashCode();
+            hashCode = hashCode * -1521134295 + Fee.GetHashCode();
+            return hashCode;
+        }
+    }
+
     public class ParkingFeeBiz
     {
+        private int _minutesInHalfHour { get; } = 30;
         private int _minutesInOneHour { get; } = 60;
-
         private decimal _zeroFee { get; } = 0m;
+        private decimal _oneDayFeeLimit { get; } = 50m;
 
+        // 傳入的是時間起迄, 並非分鐘數
+        public IEnumerable<SingleDayFee> CalcFeeForMultiDays_STD(DateTime start, DateTime end)
+        {
+            if (start > end)
+            {
+                throw new Exception("起始日期早於結束日期");
+            }
+
+            var result = new List<(DateTime start, DateTime end)>();
+
+            if (IsSameDay(start, end))
+            {
+                result.Add((start, end));
+            }
+            else
+            {
+                result.AddRange(TimeTable(start, end));
+            }
+
+            return result.Select(dtRange => new SingleDayFee()
+            {
+                StartTime = dtRange.start,
+                EndTime = dtRange.end,
+                Fee = (int)CalcFee(dtRange.start, dtRange.end)
+            });
+        }
+
+        private List<(DateTime start, DateTime end)> TimeTable(DateTime start, DateTime end)
+        {
+            DateTime startDateOnly = DateOnly(start);
+            DateTime endDateOnly = DateOnly(end);
+
+            var result = new List<(DateTime start, DateTime end)>();
+            InsertStart();
+            InsertRange();
+            InsertEnd();
+            return result;
+
+            void InsertStart()
+            {
+                result.Add((start, DayEndTime(start)));
+            }
+
+            void InsertRange()
+            {
+                var loop = Enumerable.Range(1, endDateOnly.Subtract(startDateOnly).Days - 1)
+                .Select(offset => startDateOnly.AddDays(offset))
+                .Select(dt => (dt, DayEndTime(dt)));
+
+                result.AddRange(loop);
+            }
+
+            void InsertEnd()
+            {
+                result.Add((endDateOnly, end));
+            }
+        }
+
+        private bool IsSameDay(DateTime start, DateTime end)
+        {
+            return DateOnly(start) == DateOnly(end);
+        }
+
+        private DateTime DayEndTime(DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 0);
+        }
+
+        private DateTime DateOnly(DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day);
+        }
+
+        #region Q2 Code
         public decimal CalcFee(DateTime start, DateTime end)
         {
             int totalMinutes = MinuteStat(start, end);
+
+            // Q3 新增，避免跨日計算停一整天還要往下跑計算
+            if (IsFullDay(totalMinutes))
+                return _oneDayFeeLimit;
+
             if (IsFree(totalMinutes))
                 return _zeroFee;
 
@@ -19,35 +131,43 @@ namespace Parking
             return FeeStat(hoursFee, minutesFee);
         }
 
-        private bool IsFree(int minute)
+        private bool IsFullDay(int totalMinutes)
         {
-            return (minute >= 0 && minute <= 10);
+            // Allen 老師 Q2 提示一整天為 1439 分鐘
+            int MinuutesInOneDay = 1439;
+            return (totalMinutes == MinuutesInOneDay);
         }
 
-        private decimal HourFeeStat(int minutes)
+        private bool IsFree(int totalMinutes)
+        {
+            return (totalMinutes >= 0 && totalMinutes <= 10);
+        }
+
+        private decimal HourFeeStat(int totalMinutes)
         {
             decimal hourFee = 10;
-            return (minutes / _minutesInOneHour) * hourFee;
+            return (totalMinutes / _minutesInOneHour) * hourFee;
         }
 
-        private decimal MinuteFeeStat(int minutes)
+        private decimal MinuteFeeStat(int totalMinutes)
         {
-            int mm = minutes % _minutesInOneHour;
+            int mm = totalMinutes % _minutesInOneHour;
             if (mm == 0)
                 return _zeroFee;
 
             decimal firstHourFee = 7m;
             decimal secondHourFee = 10m;
-            return mm <= 30 ? firstHourFee : secondHourFee;
+            return mm <= _minutesInHalfHour ? firstHourFee : secondHourFee;
         }
 
         private decimal FeeStat(decimal hourFee, decimal minutesFee)
         {
-            decimal oneDayFeeLimit = 50;
             decimal feeStat = hourFee + minutesFee;
-            return feeStat > oneDayFeeLimit ? oneDayFeeLimit : feeStat;
+            return Math.Min(_oneDayFeeLimit, feeStat);
         }
+        #endregion
 
+        #region Q1 Code
         public int MinuteStat(DateTime startDate, DateTime endDate)
         {
             DateTime startDateTrim = TrimSecond(startDate);
@@ -60,5 +180,6 @@ namespace Parking
         {
             return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
         }
+        #endregion
     }
 }
---
+++ Parking/Q3Test.cs
@@ -0,0 +1,243 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    [TestFixture]
+    public class Q3Test
+    {
+        private ParkingFeeBiz biz { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            biz = new ParkingFeeBiz();
+        }
+
+        #region Q3 標準
+        [TestCase("2022/5/1 09:00:00", "2022/5/1 09:10:59", 0, 1)] // 同一天
+        [TestCase("2022/5/1 09:00:00", "2022/5/1 09:11:59", 7, 1)] // 同一天
+        [TestCase("2022/5/1 09:00:00", "2022/5/1 10:00:59", 10, 1)] // 同一天
+        [TestCase("2022/5/1 23:49:00", "2022/5/2 00:10:59", 0, 2)] // 跨一天,免費
+        [TestCase("2022/5/1 23:48:00", "2022/5/2 00:00:00", 7, 2)] // 跨一天,收費
+        [TestCase("2022/5/1 23:48:00", "2022/5/2 00:11:59", 14, 2)] // 跨一天,收費
+        [TestCase("2022/5/1 00:00:00", "2022/5/2 00:11:59", 57, 2)] // 跨一天,收費
+        [TestCase("2022/5/1 23:49:00", "2022/5/3 00:11:59", 57, 3)] // 跨2天,收費
+        [TestCase("2022/5/1 22:59:00", "2022/5/3 00:11:59", 67, 3)] // 跨2天,收費
+        [TestCase("2022/5/1 00:00:00", "2022/5/3 00:11:59", 107, 3)] // 跨2天,收費
+        public void AllenKuoTest(string startValue, string endValue, int expectedFee, int expectedDays)
+        {
+            DateTime start = DateTime.Parse(startValue);
+            DateTime end = DateTime.Parse(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end);
+
+            Assert.AreEqual(expectedFee, actual.Sum(x => x.Fee));
+            Assert.AreEqual(expectedDays, actual.Count());
+        }
+
+        [Test]
+        [TestCase("2022-5-2", "2022-5-1")]
+        [TestCase("2022-5-1 00:01:00", "2022-5-1 00:00:00")]
+        public void 結束日期早於起始日期_Exception(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var ex = Assert.Throws<Exception>(() => biz.CalcFeeForMultiDays_STD(start, end));
+            StringAssert.Contains("早於", ex.Message);
+        }
+
+        [Test]
+        [TestCase("2022/5/1 00:00:00", "2022/5/2 00:11:59")] // 57
+        public void 跨一天_Case1(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,0,0,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 50 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,0,11,59) ,
+                    Fee = 7 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("2022/5/1 23:48:00", "2022/5/2 00:00:00")] // 7
+        public void 跨一天_Case2(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,23,48,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 7 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,0,0,0) ,
+                    Fee = 0 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("2022/5/1 23:48:00", "2022/5/2 00:11:59")] // 14
+        public void 跨一天_Case3(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,23,48,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 7 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,0,11,59) ,
+                    Fee = 7 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("2022/5/1 23:49:00", "2022/5/2 00:10:59")] // 0
+        public void 跨一天_時間極短_0(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,23,49,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 0 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,0,10,59) ,
+                    Fee = 0 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase("2022/5/1 00:00:00", "2022/5/3 00:11:59")] // 107
+        public void 跨兩天_Case1(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,0,0,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 50 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,23,59,0) ,
+                    Fee = 50 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,3,0,0,0) ,
+                    EndTime = new DateTime(2022,5,3,0,11,59) ,
+                    Fee = 7 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase("2022/5/1 22:59:00", "2022/5/3 00:11:59")] // 67
+        public void 跨兩天_Case2(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,22,59,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 10 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,23,59,0) ,
+                    Fee = 50 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,3,0,0,0) ,
+                    EndTime = new DateTime(2022,5,3,0,11,59) ,
+                    Fee = 7 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase("2022/5/1 23:49:00", "2022/5/3 00:11:59")] // 57
+        public void 跨兩天_Case3(string startValue, string endValue)
+        {
+            DateTime start = Convert.ToDateTime(startValue);
+            DateTime end = Convert.ToDateTime(endValue);
+
+            var actual = biz.CalcFeeForMultiDays_STD(start, end).ToList();
+
+            var expected = new List<SingleDayFee>()
+            {
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,1,23,49,0) ,
+                    EndTime = new DateTime(2022,5,1,23,59,0) ,
+                    Fee = 0 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,2,0,0,0) ,
+                    EndTime = new DateTime(2022,5,2,23,59,0) ,
+                    Fee = 50 },
+                new SingleDayFee() {
+                    StartTime = new DateTime(2022,5,3,0,0,0) ,
+                    EndTime = new DateTime(2022,5,3,0,11,59) ,
+                    Fee = 7 }
+
+            };
+
+            CollectionAssert.AreEqual(expected, actual);
+        } 
+        #endregion
+
+
+    }
+}
